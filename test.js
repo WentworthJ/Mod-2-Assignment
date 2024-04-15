aca3477ae64ab709686b5a7a8d90c3ac827e58ca
Jason Wentworth <JasonWentworth@student.olympic.edu>
2024-04-14 21:01:46 -0700

    did module 2

---
+++ test.js
@@ -0,0 +1,12 @@
+/* 3 Examples of basic arithmetic in JS */
+console.log(2 + 3);
+console.log(5%4);
+console.log(5*6);
+/* 2 Examples of declaring variables in JS */
+let a = 3;
+let b = 12;
+/* 2 Examples of using variables in arithmetic operations in JS */
+
+console.log(b/a);
+console.log(b + 8);
+console.log(a / 2);
