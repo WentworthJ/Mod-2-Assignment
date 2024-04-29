512fee31f1bb59f897d895f20242ac7321b312aa
Jason Wentworth <JasonWentworth@student.olympic.edu>
2024-04-29 03:36:22 -0700

    worked on mod 4

--- test.js
+++ test.js
@@ -1,12 +1,37 @@
-/* 3 Examples of basic arithmetic in JS */
-console.log(2 + 3);
-console.log(5%4);
-console.log(5*6);
-/* 2 Examples of declaring variables in JS */
-let a = 3;
-let b = 12;
-/* 2 Examples of using variables in arithmetic operations in JS */
+// Define JSON payload
+let games = {
+    "items": [
+        { "name": "Magicka", "price": 9.99 },
+        { "name": "Dungeon Defenders", "price": 14.99 },
+        { "name": "Goat Simulator", "price": 9.99 },
+        { "name": "Helldivers 2", "price": 39.99 }
+    ]
+}
 
-console.log(b/a);
-console.log(b + 8);
-console.log(a / 2);
+let jsonString = JSON.stringify(games);
+
+games = JSON.parse(jsonString);
+
+const table = document.getElementById("gamelist-items");
+
+// Loop through all of the items in the JSON and extract the data and put it into a table 
+
+for (i in games.items){
+    const row = document.createElement("tr");
+
+    const cell1 = document.createElement("td");
+    cell1.textContent = games.items[i].name
+    row.appendChild(cell1);
+
+    const cell2 = document.createElement("td");
+    cell2.textContent = games.items[i].price
+    row.appendChild(cell2);
+
+    table.appendChild(row);
+
+
+}
+
+
+
+
